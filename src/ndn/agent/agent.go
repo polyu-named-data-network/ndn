e49262a776db16396c7411162788264452258d5a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-03-21 23:35:12 +0800

    updated package path; added run script

---
+++ src/ndn/agent/agent.go
@@ -0,0 +1,10 @@
+package agent
+
+import "fmt"
+
+func Init() {
+	fmt.Println("init")
+	/* load from config file */
+	/* start socket service for client */
+	/* contact peer / neighbour from config file */
+}
