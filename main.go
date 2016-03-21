419124886c57c64c36b7b20ba77dacf10102fe1c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-03-21 20:23:00 +0800

    adding comment for the overall flow; to read from config file

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bitbucket.org/polyu-named-data-network/ndn-agent/ndn/ndn-agent"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	ndn_agent.Init()
 }
