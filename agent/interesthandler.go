3dc850c56af8df92545dcd8323fb9175fa60b0a8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-18 14:43:36 +0800

    updated debug message func call

--- agent/interesthandler.go
+++ agent/interesthandler.go
@@ -3,6 +3,7 @@ package agent
 import (
   "bitbucket.org/polyu-named-data-network/ndn/fib"
   "bitbucket.org/polyu-named-data-network/ndn/packet"
+  "bitbucket.org/polyu-named-data-network/ndn/utils"
   "encoding/json"
   "fmt"
   "io"
@@ -15,13 +16,13 @@ type interestHandler_s struct {
 }
 
 func (interestHandler_s) HandleError(err error) {
-  fmt.Println("falied to listen on incoming interest socket", err)
+  utils.Error("failed to listen on incoming interest socket", err)
 }
 
 /* REMARK: this function is blocking */
 func (p interestHandler_s) HandleConnection(conn net.Conn) {
   p.wg.Add(1)
-  fmt.Println("client connected to interest service", conn.RemoteAddr().Network(), conn.RemoteAddr().String())
+  utils.Info("client connected to interest service", conn.RemoteAddr().Network(), conn.RemoteAddr().String())
 
   go func() {
     defer p.wg.Done()
@@ -29,20 +30,21 @@ func (p interestHandler_s) HandleConnection(conn net.Conn) {
     decoder := json.NewDecoder(conn)
     var in_packet packet.InterestPacket_s
     for err == nil {
-      fmt.Println("wait for incoming interest packet")
+      utils.Info("wait for incoming interest packet")
       err = decoder.Decode(&in_packet)
       if err != nil {
         if err != io.EOF {
-          fmt.Println("failed to decode incoming interest packet", err)
+          utils.Error("failed to decode incoming interest packet", err)
         }
       } else {
-        fmt.Println("received interest packet")
+        utils.Info("received interest packet", in_packet)
         /*  find data, response if found, otherwise do forwarding
          *    1. lookup CS
          *    2. lookup PIT
          *    3. lookup FIB
          *    4. calculate forwarding port according to algorithm for unknown cases
          */
+
         csFound := false
         pitFound := false
         if csFound {
